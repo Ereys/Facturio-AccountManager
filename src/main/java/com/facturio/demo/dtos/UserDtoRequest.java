b5d6165fd3d7fac4d0eb6d317386c4bb2a971059
Awat <taviran@gmail.com>
2023-03-29 12:18:31 +0400

    deboginig

--- src/main/java/com/facturio/demo/dtos/UserDtoRequest.java
+++ src/main/java/com/facturio/demo/dtos/UserDtoRequest.java
@@ -3,7 +3,8 @@ package com.facturio.demo.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data @AllArgsConstructor
+@Data
+@AllArgsConstructor
 public class UserDtoRequest {
     private String name;
     private String password;
--- src/main/java/com/facturio/demo/repositories/UserRepository.java
+++ src/main/java/com/facturio/demo/repositories/UserRepository.java
@@ -1,12 +1,12 @@
 package com.facturio.demo.repositories;
 
-import com.facturio.demo.entities.User;
+import com.facturio.demo.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findUserByName(String name);
-    public User findUsersById(String id);
-    public List<User> findUserByNameStartsWith(String pattern);
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    public AppUser findUserByName(String name);
+
+    public List<AppUser> findUserByNameStartsWith(String pattern);
 }
